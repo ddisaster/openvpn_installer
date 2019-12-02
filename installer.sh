ed1509e4244d23f0615f0d9aa018afd7ce022933
ddisaster <ddisaster@users.noreply.github.com>
2019-12-02 20:57:20 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -115,6 +115,7 @@ echo "export KEY_OU=\"${ounit}\"" >> /etc/openvpn/easy-rsa/vars || error
 openssl dhparam -out /etc/openvpn/dh2048.pem 2048 || error
 
 cp ${tempfolder}/openssl-1.0.0.cnf /etc/openvpn/easy-rsa/openssl.cnf || error
+cp ${tempfolder}/whichopensslcnf /etc/openvpn/easy-rsa/whichopensslcnf || error
 
 cd /etc/openvpn/easy-rsa || error
 source vars || error
